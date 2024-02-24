f59ff0ba7cdf63f9b10ce237ef16878004438cad
[KyryloPoidiuk] <kirilpojduk@gmail.com>
2024-02-24 16:52:13 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import http, { get } from "http"
 import fs from "fs"
 import path from "path"
-import { fileURLToPath } from "url"
+import { fileURLToPath, parse } from "url"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -12,12 +12,19 @@ const server = http.createServer(function (req, res) {
     if (req.url == "/jokes" && req.method == "GET") {
         getAllJokes(req, res)
     }
-    if (req.url == "/jokes" && req.method == "POST") {
+    else if (req.url == "/jokes" && req.method == "POST") {
         addNewJoke(req, res)
     }
-    // else{
-    //     res.end("<h1>404</h1>")
-    // }
+    else if (req.url.startsWith("/like") && req.method == "GET") {
+        like(req,res)
+    }
+    else if (req.url.startsWith("/dislike") && req.method == "GET") {
+        dislike(req,res)
+    }
+
+    else {
+        res.end("<h1>404</h1>")
+    }
 })
 
 server.listen(3333);
@@ -51,4 +58,32 @@ function addNewJoke(req, res) {
 
         res.end()
     })
+}
+
+function like(req, res) {
+    let params = parse(req.url, true).query
+    let id = params.id
+    if (id) {
+        let filepath = path.join(dataPath, id + ".json")
+        let file = fs.readFileSync(filepath)
+        file = Buffer.from(file).toString()
+        let joke = JSON.parse(file)
+        joke.like++;
+        fs.writeFileSync(filepath, JSON.stringify(joke))
+    }
+    res.end()
+}
+
+function dislike(req, res) {
+    let params = parse(req.url, true).query
+    let id = params.id
+    if (id) {
+        let filepath = path.join(dataPath, id + ".json")
+        let file = fs.readFileSync(filepath)
+        file = Buffer.from(file).toString()
+        let joke = JSON.parse(file)
+        joke.dislikes++;
+        fs.writeFileSync(filepath, JSON.stringify(joke))
+    }
+    res.end()
 }
